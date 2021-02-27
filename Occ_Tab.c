67c28c9ee1f7940d86c78ab638efd61b4bf562b3
Léa Yeromonahos <51425332+Araenn@users.noreply.github.com>
2021-02-27 11:47:15 +0100

    Create Occ_Tab.c

---
+++ Occ_Tab.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX 50
+void TypeTab (int *Tab, int n);
+void ShowTab(int *Tab, int n);
+int PackTab(int *Tab , int n);
+
+
+main()
+{
+    int u[MAX];
+    int n,m;
+
+    do
+    {
+        printf("Choose the tab size (MAX.%d) : ", MAX);
+        scanf("%d", &n );
+    }
+    while(n>MAX);
+    {
+        TypeTab(u,n);
+        ShowTab(u,n);
+        m = PackTab(u,n);
+        ShowTab(u ,m);
+    }
+}
+void TypeTab(int *Tab, int n) 
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        printf("Choose the n°%d value, element of u", i);
+        scanf("%d", &Tab[i]);
+    }
+
+}
+void ShowTab(int *Tab, int n)
+{
+    printf("Tab : \n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", Tab[i]);
+        printf("\n");
+    }
+
+}
+int PackTab(int *Tab , int n) /*The PackTab function will be responsible for packing the elements in the Tab table (thus changing its contents).
+and will return the new number of elements in the packed array (without the null values). */
+{
+    int j = 0;
+    for(int i = 0; i < n; i++)
+    {
+        Tab[j] = Tab[i];
+        if(Tab[j])
+        {
+            j++;
+        }
+    }
+    return j;
+}
